f6bf509b3fa3504dbbec7266610e8c66db19247a
jxr2006 <xxx.com>
2023-03-25 21:59:31 +0800

    完善递归获得场景依赖

--- get_all_scripts_in_scene.py
+++ get_all_scripts_in_scene.py
@@ -2,7 +2,7 @@ import UnityPy
 from pathlib import Path
 
 BASE_PATH = Path(r"D:/ArkAssets")
-SCENE_PATH = 'scenes/obt/main/level_main_02-01/level_main_02-01.ab'
+SCENE_PATH = 'scenes/obt/main/level_main_01-07/level_main_01-07.ab'
 
 manifest_env = UnityPy.load(str(BASE_PATH / 'torappu.ab'))
 
@@ -13,22 +13,32 @@ for obj in manifest_env.objects:
 def get_deps(env: UnityPy.Environment, path) -> list[str]:
     dep_names = []
 
-    for obj in env.objects:
-        if obj.type.name == 'PreloadData':
-            dep_names += obj.read_typetree()['m_Dependencies']
+    def get(env: UnityPy.Environment, path):
+        nonlocal dep_names
+        temp_names = []
 
-    for pair in manifest['AssetBundleNames']:
-        if pair[1] == path:
-            first = pair[0]
+        for obj in env.objects:
+            if obj.type.name == 'PreloadData':
+                for n in obj.read_typetree()['m_Dependencies']:
+                    if n not in dep_names: temp_names.append(n)
 
-    for info in manifest['AssetBundleInfos']:
-        if info[0] == first:
-            deps = info[1]['AssetBundleDependencies']
+        for pair in manifest['AssetBundleNames']:
+            if pair[1] == path:
+                first = pair[0]
 
-    for pair in manifest['AssetBundleNames']:
-        if pair[0] in deps:
-            if pair[1] not in dep_names: dep_names.append(pair[1])
+        for info in manifest['AssetBundleInfos']:
+            if info[0] == first:
+                deps = info[1]['AssetBundleDependencies']
 
+        for pair in manifest['AssetBundleNames']:
+            if pair[0] in deps:
+                if pair[1] not in dep_names and pair[1] not in temp_names: temp_names.append(pair[1])
+
+        dep_names += temp_names
+        for n in temp_names:
+            get(env, n)
+
+    get(env, path)
     return dep_names
 
 def get_scripts(env: UnityPy.Environment) -> list[str]:
@@ -41,9 +51,11 @@ def get_scripts(env: UnityPy.Environment) -> list[str]:
 
 main_scene_env = UnityPy.load(str(BASE_PATH / SCENE_PATH))
 scripts = get_scripts(main_scene_env)
+print('loaded main scene: ' + SCENE_PATH)
 for dep in get_deps(main_scene_env, SCENE_PATH):
     dep_env = UnityPy.load(str(BASE_PATH / dep))
     scripts += get_scripts(dep_env)
+    print('loaded dep: ' + dep)
 
 scripts = set(scripts) #去重
 
